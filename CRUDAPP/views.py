fb53dc730369e7f3308fc8c7cfc526767758f203
mwizard9 <manishhimalaya64@gmail.com>
2022-06-26 16:35:17 +0545

    models

--- CRUDAPP/views.py
+++ CRUDAPP/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from readline import insert_text
+from django.shortcuts import render, redirect
+from .forms import EmployeeForm
+from .models import Employee
 
-# Create your views here.
+# Create your views here
+def insert_emp(request):
+
+
+def show_emp(request):
+
+
+def edit_emp(request):
+
+
+def remove_emp(request):
