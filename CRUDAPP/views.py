919c2719d417882872d4b0325ad2d3feee07c630
mwizard9 <manishhimalaya64@gmail.com>
2022-06-26 16:55:05 +0545

    views

--- CRUDAPP/views.py
+++ CRUDAPP/views.py
@@ -1,16 +1,21 @@
 from readline import insert_text
 from django.shortcuts import render, redirect
-from .forms import EmployeeForm
+from CRUDAPP .models import Employee
 from .models import Employee
 
 # Create your views here
 def insert_emp(request):
+    if request == "POST":
+        EmpId = request.POST['EmpId']
+        EmpName = request.POST['EmpName']
+        EmpGender = request.POST['EmpGender']
+        EmpEmail = request.POST['EmpEmail']
+        EmpDesignation = request.POST['EmpDesignation']
+        data = Employee(EmpId=EmpId, EmpName=EmpName, EmpGender=EmpGender, EmpEmail=EmpEmail, EmpDesignation=EmpDesignation)
+        data.save()
 
+        return redirect('show/')
+    else:
+        return render(request,'insert.html')
+        
 
-def show_emp(request):
-
-
-def edit_emp(request):
-
-
-def remove_emp(request):
